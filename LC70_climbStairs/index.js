b9869c4d6d694b9a19cf3033f3cb5c956c1e7a52
Jeffrey Nguyen <jeffmiguel7@gmail.com>
2020-04-03 20:40:05 -0700

    number of ways to climb the stairs to the nth step

--- LC70_climbStairs/index.js
+++ LC70_climbStairs/index.js
@@ -1,3 +1,19 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+  let fib_1 = 1;
+  let fib_2 = 2;
+  let fib_fin = 0;
+
+  if (n <= 3) {
+    return n;
+  }
+
+  for (let step = 3; step <= n; step++) {
+    fib_fin = fib_1 + fib_2;
+    fib_1 = fib_2;
+    fib_2 = fib_fin;
+  }
+
+  return fib_fin;
+};
 
 module.exports = climbStairs;
